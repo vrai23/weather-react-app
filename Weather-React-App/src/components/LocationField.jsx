298f38d130733f0b712581283f62f2b1beb1873e
Vachan Rai <rai.vachan@yahoo.com>
2024-05-17 19:24:20 -0400

    changes

--- Weather-React-App/src/components/LocationField.jsx
+++ Weather-React-App/src/components/LocationField.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
+const cityNameField = {
+
+  borderRadius: "15px" ,
+  borderColor: "#5157EB" ,
+width: "266px" ,
+height: "58px" ,
+
+}
+
 function LocationField({ location, setLocation, searchLocation }) {
   return (
     <div className="search">
       <input
+      style= {cityNameField}
         value={location}
         onChange={(event) => setLocation(event.target.value)}
         onKeyPress={searchLocation}
